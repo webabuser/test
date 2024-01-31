abb688b2a92a5f9b13e9fdba90a0a5874f2b647d
webabuser <vitiya@inbox.ru>
2024-01-31 14:09:06 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "chat.h"
+
+using namespace std;
+
+int main()
+{
+    Chat chat;
+    chat.Start();
+    
+    
+    cout<<"Программа чат завершена!" << std::endl;
+
+    return 0;
+}
